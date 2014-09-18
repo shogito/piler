9d48d22a65959748f303cceb7331a5c5d652ec0b
shogito <pijo.eng@gmail.com>
2014-09-19 01:10:31 +0900

    2014.09.19 piler.openのテストを実装

--- js/piler.js
+++ js/piler.js
@@ -1,117 +1,101 @@
 "use strict"
 // underscore.jsに依存
-function piler(_pile){
-    // _pileが配列以外なら例外だせ
-    function verifyPile(_pile){
-        if ( ! (_pile instanceof Array) ){
+function piler(pile){
+    // pileが配列以外なら例外出す
+    function verifyPile(pile){
+        if ( ! (pile instanceof Array) ){
             throw "error: argument is not an Array"
         }
     }
+    verifyPile(pile);
 
-    verifyPile(_pile);
-
-    var _initPile = [].concat(_pile); // 初期状態をキャッシュ
-    var _length = function(){         // getterメソッドとして実装
-        return _pile.length ; 
+    var initPile = [].concat(pile); // 初期状態をキャッシュ
+    var length = function(){         // getterメソッドとして実装
+        return pile.length ; 
     } 
     
-
-    var _maxIdx = _pile.length - 1;   // いらなくね
-
-    var updateLength = function(){    // いらなくね
-        _length = _pile.length;       
-        _maxIdx = _pile.length - 1;
+    // underscore.jsで書き換える
+    var random = function(num){
+        return Math.floor(Math.random() * num);
     }
-    
-    var random = function(_num){
-        return Math.floor(Math.random() * _num);
-    }
-    
-    var showIndexes = function(){    // いらなくね
-        return _pile.length;
-    }
-    
-    var shuffle = function(){
-        _pile = _.shuffle(_pile);
-    }
-    
-
-    var draw = function(_num){     // 引数なしで全部引く。  
-        if(_num === undefined){    // === undefined使うってどうよ？
-            _num = 0;
-        }       
-        if(_length > 0){
-            var _drawed = _pile.splice(_num, 1)[0];
-            updateLength();
-            return _drawed;
-        }
-        return "empty";　　// try:catchで書いたほうがいい
-    }
-
-
-
-    var randomDraw = function(){    // drawの_numに乱数ぶっこめばよくね
-        _num = random(_maxIdx);        
-        if(_length > 0){
-            var _drawed = _pile.splice(_num, 1)[0];
-            updateLength();
-            return _drawed;
-        }
-        return "empty";
-    }
-    
-
-    var reset = function(){
-        _pile = [].concat(_initPile);
-        updateLength();
-    }
-    
-    var open = function(_idx, _num){　　　　// 引数なしで全部開く。
-        _idx = _idx - 1 || 0;
-        _num = arguments[1] || 1;
         
-        return _pile.slice(_idx, _idx + _num);
+    var shuffle = function(){
+        pile = _.shuffle(pile);
+    }
+    
+    var open = function(_idx, num){
+        _idx = _idx || 0;
+        num = arguments[1] || pile.length;        
+        return pile.slice(_idx, _idx + num);
     }
 
-    var reverseOpen = function(_idx, _num){    // _.jsで非破壊ソートしようよ　-> indexOfで探せばよくね
-        _idx = _idx - 1 || 0;
-        _num = arguments[1] || 0;
-        var _rsl = new Array(_num);
+    var reverseOpen = function(_idx, num){    // _.jsで非破壊ソートしようよ　-> indexOfで探せばよくね
+        _idx = _idx  || 0;
+        num = arguments[1] || 0;
+        var _rsl = new Array(num);
         var _start = _maxIdx - _idx;
         
-        for (var _i = 0; _i < _num ; _i++){
-            _rsl[_i] = _pile[_start - _i];
+        for (var _i = 0; _i < num ; _i++){
+            _rsl[_i] = pile[_start - _i];
         }
         
         return _rsl;
     }
+
+
+
+    var draw = function(num){     // 引数なしで全部引く。  
+        if(num === undefined){    // === undefined使うってどうよ？
+            num = 0;
+        }       
+        if(length > 0){
+            var drawed = pile.splice(num, 1);
+            return drawed;
+        }
+    }
+
+    var drawRandom = function(num){    // drawのnumに乱数ぶっこめばよくね
+        var drawed = []   
+        for (var i = 0; num > i; i++){    
+            if(pile.length > 0){
+                drawed += draw(_.random(0,pile.length));
+            }
+        }
+        return drawed;
+    }
+    
+    var reset = function(){
+        pile = [].concat(_initPile);
+        updateLength();
+    }
+    
     
     var put = function(_put, _idx){    
-        _pile = _pile.concat(_put);
+        pile = pile.concat(_put);
         updateLength();                   //  iranai
         return _put;
     }
     
-    var cut = function(_num){
-        var _cut = _pile.splice(0, Math.ceil(_length/2));
+    var cut = function(num){
+        var _cut = pile.splice(0, Math.ceil(length/2));
         updateLength();                   // iranai
         return piler(_cut);
     }
     
-    var concatPile = function(_pileObj){
-        _pile = _pile.concat(_pileObj.opennAll());
+    var concatPile = function(pileObj){
+        pile = pile.concat(pileObj.opennAll());
         updateLength();
-//        delete _pileObj;  // global objectをdeleteする必要がある
+//        delete pileObj;  // global objectをdeleteする必要がある
     }
 
 
     var uniqObjDraw = function(obj){    // drawTargetObj
-        return draw(_pile.indexOf(obj));
+        return draw(pile.indexOf(obj));
         
     } 
 
     var uniqObjSearch = function(obj){  // getObjIndex(同値は探せない)
-        return _pile.indexOf(obj);
+        return pile.indexOf(obj);
         
     } 
 
@@ -120,17 +104,17 @@ function piler(_pile){
 // _で書き換えろ[配列で帰ってくる]    
     var search = function(fn){     // _のフィルターかなんかで書き換えよう. ex) function(x){if(x.num === 1){return x}} 
         var _rsl;
-        for (var _i = 0; _i < _length; _i++){
-            if (fn(_pile[_i])){
+        for (var _i = 0; _i < length; _i++){
+            if (fn(pile[_i])){
                 return _i;
             }
         }
     } 
     
 // _で書き換えろ
-    var uniqNumberSearch = function(_num){
+    var uniqNumberSearch = function(num){
         var fn = function(x){
-            if(x === _num){
+            if(x === num){
                 return true;
             }
         }
@@ -141,16 +125,16 @@ function piler(_pile){
         // プロパティ
 
         // メソッド
-        length: _length,
+        length: length,
         uniqObjDraw: uniqObjDraw,
         uniqObjSearch: uniqObjSearch,
         draw: draw,
-        randomDraw: randomDraw,
+        drawrRandom: drawRandom,
         shuffle: shuffle,
         reset: reset,
         open: open,
         rOpen: reverseOpen,
-        openAll: function() { return open(1, _length);},
+        openAll: function() { return open(1, length());},
         put: put,
         cut: cut,
         concat: concatPile
--- spec/pilerTest.js
+++ spec/pilerTest.js
@@ -1,3 +1,9 @@
+describe("_のロード確認", function(){
+	it("_のロード確認", function(){
+		expect(_).toBeDefined;
+	})
+})
+
 describe("pilerの初期化テスト",function(){
     it("配列オブジェクト",function(){
     	var p = piler([1,2,3,4,5])
@@ -30,7 +36,6 @@ describe("piler.lengthのテスト", function(){
     })
 
     // 更新系実行後のテスト書け
-    // プロパティだと外から更新かけられるからからやっぱgetterにしたほうがいいかも
 
 })
 
@@ -38,19 +43,63 @@ describe("piler.lengthのテスト", function(){
 
 describe("openメソッドのテスト",function(){
     beforeEach(function(){
-        var p = [1,2,3,4,5];
-        var pile = piler(p);
+        this.p = [1,2,3,4,5];
+        this.pile = piler(this.p);
 
     })
     // openはpileのlenghtを変動させない(非破壊)
     // 引数なし -> 全て表示する
- 
-    // 引数一つあり -> 対象のindexのオブジェクトだけ[]でwrapしてかえす
+    it("pile.open(引数なし)",function(){
+    	expect(this.pile.open()).toEqual(this.p);
+    	expect(this.pile.length()).toEqual(5);
+    })
+    // 引数一つあり -> 対象のindexのオブジェクトから一番後ろまで[]でwrapしてかえす
+    it("pile.open(0)",function(){
+    	expect(this.pile.open(2)).toEqual([3,4,5]);
+    	expect(this.pile.length()).toEqual(5);   	
+    })
 
     // 引数一つあり(out of range) -> []を返す
+    it("pile.open(out of range)",function(){
+    	expect(this.pile.open(5)).toEqual([]);
+    	expect(this.pile.length()).toEqual(5);   	
+    })
+
+    // 引数ふたつで一個だけ -> 第一引数のindexから第二引数個返す
+    it("pile.open(2,1)",function(){
+    	expect(this.pile.open(2,1)).toEqual([3]);
+    	expect(this.pile.length()).toEqual(5);   	
+    })
 
     // 引数ふたつ -> 第一引数から第二引数個分配列で返す
+    it("pile.open(2,3)",function(){
+    	expect(this.pile.open(2,3)).toEqual([3,4,5]);
+    	expect(this.pile.length()).toEqual(5);   	
+    })
 
-    // 配列数よりも引数に渡したindexが多い場合
+    // 引数ふたつ(第一引数が最大indexと一緒)
+    it("pile.open(this.pile.length() - 1,3)",function(){
+    	expect(this.pile.open(this.pile.length() - 1,3)).toEqual([5]);
+    	expect(this.pile.length()).toEqual(5);   	
+    })
+
+    // 引数ふたつで第一引数がマイナス方向にout of range
+    it("pile.open(-1,5)",function(){
+    	expect(this.pile.open(-1,5)).toEqual([]);
+    	expect(this.pile.length()).toEqual(5);   	
+    })
+
+    // 引数ふたつで第一引数がプラス方向にout of range
+    it("pile.open(5,5)",function(){
+    	expect(this.pile.open(5,5)).toEqual([]);
+    	expect(this.pile.length()).toEqual(5);   	
+    })
+
+    // 配列数よりも第二引数に渡したindexがout of range
+    it("pile.open(5,5)",function(){
+    	expect(this.pile.open(2,18)).toEqual([3,4,5]);
+    	expect(this.pile.length()).toEqual(5);   	
+    })
+
 
 })
